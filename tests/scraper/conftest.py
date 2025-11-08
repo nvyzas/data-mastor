cf9dc9b1bfca00d00c869d1e05c3dceb74acf69d
Nikolaos Vyzas <nbyzas@gmail.com>
2025-11-08 12:16:32 +0200

    test: fix in-memory db fails and refactor conftest

---
+++ tests/scraper/conftest.py
@@ -0,0 +1,15 @@
+# pyright: reportUnusedImport=false
+from data_mastor.scraper.testing import (
+    configure_spidercls,
+    engine,
+    entities,
+    extension_modules,
+    fill_with_entities,
+    gatherercls,
+    reset_db,
+    sessmkr,
+    setup_db_url,
+    spidercls,
+    testcase_dir,
+    yamlargs,
+)
--- tests/scraper/test_pipelines.py
+++ tests/scraper/test_pipelines.py
@@ -1,7 +1,7 @@
 import pytest
 from scrapy.exceptions import DropItem
 from sqlalchemy import select
-from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.session import Session, sessionmaker
 
 from data_mastor.scraper.models import Listing, Source
 from data_mastor.scraper.pipelines import ListingStorer, SourceStorer, process_items
@@ -12,21 +12,30 @@ s1 = SourceItem("lvl1", "lvl0", 1)
 s2 = SourceItem("lvl2", "lvl1", 2)
 
 
+@pytest.fixture(autouse=True)
+def reset_db(reset_db):
+    pass
+
+
 class TestSourceStorer:
     @pytest.fixture
     def pipe(self) -> SourceStorer:
         return SourceStorer()
 
-    def test_pipe_normal(self, session: Session, pipe: SourceStorer):
+    def test_pipe_normal(
+        self, sessmkr: sessionmaker[Session], pipe: SourceStorer
+    ) -> None:
+        assert len(sessmkr().scalars(select(Source)).all()) == 0
         process_items([s0, s1, s2], pipe)
-        entities = session.scalars(select(Source)).all()
-        assert len(entities) == 3
+        assert len(sessmkr().scalars(select(Source)).all()) == 3
 
-    def test_pipe_broken_hierarchy(self, session: Session, pipe: SourceStorer):
+    def test_pipe_broken_hierarchy(
+        self, sessmkr: sessionmaker[Session], pipe: SourceStorer
+    ) -> None:
+        assert len(sessmkr().scalars(select(Source)).all()) == 0
         with pytest.raises(DropItem):
             process_items([s1, s2], pipe)
-        entities = session.scalars(select(Source)).all()
-        assert len(entities) == 0
+        assert len(sessmkr().scalars(select(Source)).all()) == 0
 
 
 l1 = ListingItem("text1", "1")
@@ -39,11 +48,10 @@ class TestListingStorer:
     def pipe(self) -> ListingStorer:
         return ListingStorer()
 
-    def test_lstpipe(self, session: Session, pipe: ListingStorer):
-        processed = process_items(listings, pipe)
-        entities = session.scalars(select(Listing)).all()
-        assert len(entities) == 2
-        assert processed == listings
+    def test_lstpipe(self, sessmkr: sessionmaker[Session], pipe: ListingStorer) -> None:
+        assert len(sessmkr().scalars(select(Listing)).all()) == 0
+        process_items(listings, pipe)
+        assert len(sessmkr().scalars(select(Listing)).all()) == 2
 
 
 if __name__ == "__main__":
