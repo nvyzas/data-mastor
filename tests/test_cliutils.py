98570b36bddf187ad71bd1606688de589a1d38d3
Nikolaos Vyzas <nbyzas@gmail.com>
2025-11-16 10:07:41 +0200

    TEMP

--- tests/test_cliutils.py
+++ tests/test_cliutils.py
@@ -79,35 +79,25 @@ def test_auto_invoke(app0: Typer, app1: Typer) -> None:
 
 
 def app_funcs_from_keys(app: Typer, keys: list[str] | None = None):
-    funcs = []
-
-    # look in the root app (first arg)
-    # its name should NOT be included in the keys arg
-    if not keys:
-        if app.registered_callback:
-            _f = app.registered_callback.callback
-            print(f"Adding callback '{_f}' from root app")
-            funcs.append(_f)
-        if app.registered_groups:
-            msg = "There are registered groups in root but no keys given"
-            raise ValueError(msg)
-        if len(app.registered_commands) > 1:
-            msg = "There are more than one commands in root but no keys given"
-            raise ValueError(msg)
-        if len(app.registered_commands) == 1:
-            _f = app.registered_commands[0].callback
-            funcs.append(_f)
-        return funcs
-
-    # iterate through keys (since all must be matched)
+    if keys is None:
+        keys = []
+    funcs: list[Callable] = []
     tpr = app
-    for i, key in enumerate(keys[:-1]):
-        # check for callback
+    i = 0
+    while True:
+        # look for callback
         if tpr.registered_callback:
-            _f = tpr.registered_commands[0].callback
-            print(f"Adding callback '{_f}' from app named '{key}'")
-            funcs.append(tpr.registered_callback.callback)
-        # traverse groups
+            if tpr.registered_callback.callback is None:
+                print(f"WARNING: Skipping NULL callback at iteration '{i}'")
+            else:
+                _f = tpr.registered_callback.callback
+                print(f"Adding callback '{_f}' at iteration '{i}'")
+                funcs.append(_f)
+        # check we reached the last key, or there is no key
+        if i >= len(keys) - 1:
+            break
+        # go to the next group
+        key = keys[i]
         key_tprs = []
         for g in tpr.registered_groups:
             if g.typer_instance is None:
@@ -120,19 +110,35 @@ def app_funcs_from_keys(app: Typer, keys: list[str] | None = None):
         if not key_tprs:
             raise ValueError(f"There is no app named '{key}' (keys: {keys})")
         tpr = key_tprs[0]
+        i += 1
 
-    # look for the command (using the last key)
-    key = keys[-1]
-    cmds = [_ for _ in tpr.registered_commands if _.callback is not None]
-    key_cmds = [_ for _ in cmds if key in [_.name, _.callback.__name__]]  # type: ignore
-    if len(key_cmds) > 1:
-        raise ValueError(f"There are more than one commands named '{key}'")
-    if not key_cmds:
-        raise ValueError(f"No command found (from the last key '{key}')")
-    funcs.append(key_cmds[0].callback)
-    return funcs
+    # look for the command
+    print(f"Looking for cmd at iteration '{i}'")
 
-    return funcs
+    # use (the last) key to find the command
+    if keys:
+        key = keys[-1]
+        cmds = [_ for _ in tpr.registered_commands if _.callback is not None]
+        key_cmds = [_ for _ in cmds if key in [_.name, _.callback.__name__]]  # type: ignore
+        if len(key_cmds) > 1:
+            raise ValueError(f"There are multiple valid commands matching '{key}'")
+        if not key_cmds:
+            raise ValueError(f"No valid command found matching '{key}')")
+        funcs.append(key_cmds[0].callback)  # type: ignore
+        return funcs
+
+    # if there are groups
+    if tpr.registered_groups:
+        raise ValueError("App contains groups, but no keys were given")
+    # if there is no command, return the funcs (containing only callbacks)
+    if not tpr.registered_commands:
+        raise ValueError("Top-level app  has no registered commands")
+    # check if it's a single command
+    if len(tpr.registered_commands) == 1:
+        if tpr.registered_commands[0].callback is None:
+            raise ValueError("The single command is invalid (has no function)")
+        funcs.append(tpr.registered_commands[0].callback)
+        return funcs
 
 
 mocks = {}
@@ -172,23 +178,60 @@ def test_cmd_from_keys_callback_and_command() -> None:
     assert funcs == [ff(1), ff(2)]
 
 
-def test_cmd_from_keys_nested():
+@pytest.fixture
+def nested():
+    # a4
+    app4 = Typer(name="a4")
+    app4.callback()(ff(40))
+    app4.command()(ff(41))
+    app4.command()(ff(42))
+    # a3
+    app3 = Typer(name="a3")
+    app3.callback()(ff(30))
+    app3.command()(ff(31))
+    app3.command()(ff(32))
+    # a2
+    app2 = Typer(name="a2")
+    app2.callback()(ff(20))
+    app2.command()(ff(21))
+    app2.add_typer(app3)
+    app2.add_typer(app4)
+    # a1
     app1 = Typer()
     app1.callback()(ff(10))
     app1.command()(ff(11))
-    app2 = Typer()
-    app2.callback()(ff(20))
-    app2.command()(ff(21))
     app1.add_typer(app2)
-    with pytest.raises(ValueError):
-        app_funcs_from_keys(app1)
-    # lvl1 command
-    assert app_funcs_from_keys(app1, ["f11"]) == [ff(10), ff(11)]
-    # lvl2 command
-    app2.info.name = "two"
-    assert app_funcs_from_keys(app1, ["two", "f21"]) == [ff(10), ff(11), ff(20), ff(21)]
-    # assert app_funcs_from_keys(app1, ["f11"]) == [ff(11)]
-    # assert app_funcs_from_keys(app1, ["f11"]) == [ff(11)]
+    return app1
+
+
+class Test_app_funcs_from_keys:
+    def test_exceptions(self, nested: Typer) -> None:
+        # contains groups, but no keys
+        with pytest.raises(ValueError, match="contains groups, but no keys"):
+            app_funcs_from_keys(nested)
+        # no valid command found matching
+        with pytest.raises(ValueError, match="No valid command found matching"):
+            app_funcs_from_keys(nested, ["a2"])
+        with pytest.raises(ValueError, match="No valid command found matching"):
+            app_funcs_from_keys(nested, ["a2", "a3"])
+        # has no registered commands
+        nested.registered_groups = []
+        nested.registered_commands = []
+        with pytest.raises(ValueError, match="has no registered commands"):
+            app_funcs_from_keys(nested)
+
+    def test_match_single_command(self, nested: Typer) -> None:
+        app_funcs_from_keys(nested, ["a2", "a3", "a4"])
+
+    def test_normal(self, nested: Typer) -> None:
+        # lvl1 command
+        assert app_funcs_from_keys(nested, ["f11"]) == [ff(10), ff(11)]
+        # lvl2 command
+        funcs = app_funcs_from_keys(nested, ["a2", "f21"])
+        assert funcs == [ff(10), ff(20), ff(21)]
+        # lvl3 command
+        funcs = app_funcs_from_keys(nested, ["a2", "a3", "f32"])
+        assert funcs == [ff(10), ff(20), ff(30), ff(32)]
 
 
 def test_cmd_from_keys_flat_with_cmd(app0):
